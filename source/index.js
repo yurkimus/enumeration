cb86a6162b190b429c1032cfbcee88ac0875b3b0
Iurii <yurkimus@gmail.com>
2024-08-31 13:31:16 +0200

    Add constructor parameters and update readme

--- source/index.js
+++ source/index.js
@@ -1,13 +1,53 @@
+import { type } from '@yurkimus/types'
+
 var Symbols = {
   Keys: Symbol('keys'),
   Values: Symbol('values'),
   Entries: Symbol('entries'),
 }
 
-export function Enumeration() {
+export function Enumeration(...init) {
   this[Symbols.Keys] = new Set()
   this[Symbols.Values] = new Set()
   this[Symbols.Entries] = new Map()
+
+  switch (init.length) {
+    case 0:
+      break
+
+    case 1:
+      switch (type(init[0])) {
+        case 'Object':
+          for (var key in init[0]) {
+            this.add(key, init[0][key])
+          }
+
+          break
+
+        case 'Array':
+          if (!(init[0].every(Array.isArray) && init[0].length === 2)) {
+            throw new TypeError(
+              `Expected array initializer to contain keys and values arrays`,
+            )
+          }
+
+          for (var index in init[0][0]) {
+            this.add(init[0][0][index], init[0][1][index])
+          }
+
+          break
+
+        default:
+          throw new TypeError(
+            `Initializer of type "${type(init[0])}" is not supported`,
+          )
+      }
+
+      break
+
+    default:
+      throw new TypeError(`Expected Enumeration init to have 0 or 1 argument`)
+  }
 }
 
 Enumeration.prototype.has = function (value) {
